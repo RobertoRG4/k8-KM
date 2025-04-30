7b0423e3abd91fbc7cd225a9e330d8a11efaffb4
Roberto Romero Guadarrama <robertoromeroguadarrama@gmail.com>
2025-04-29 21:56:13 -0700

    cors

--- api/src/main/java/com/api/config/Cors.java
+++ api/src/main/java/com/api/config/Cors.java
@@ -8,15 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class Cors {
-    @Value("${base.url.origins}")
-    private String origins;
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/v1/**")
-                        .allowedOrigins(origins)
+                        .allowedOrigins("http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*").allowCredentials(true);
             }
